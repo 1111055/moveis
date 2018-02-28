a13db3a41df3296cf1d5b954c095cefaf6c86c28
Marco Mendes <marco.mendes@sroque.pt>
2018-02-28 15:36:05 +0000

    Home Page
    Home Page

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -9,62 +9,17 @@
 <body>
 
 <!--header section -->
-<section class="banner" role="banner">
-
-<nav class="gtco-nav" role="navigation">
-    <div class="gtco-container">
-      <div class="row">
-        <div class="col-xs-8 text-center menu-1" style="width: 100% !important;">
-          <ul>
-            <li class="active"><a href="index.html">Home</a></li>
-            <li><a href="about.html">Sobre</a></li>
-            <li><a href="produtos.html">Produtos</a></li>
-            <li><a href="contact.html">Contactos</a></li>
-          </ul>
-        </div>
-      </div>
-      
-    </div>
-  </nav>
-  <div class="banner-area"> 
-    <!-- overlay -->
-    <div class="banner-area-gradient"></div>
-    <!-- overlay -->
-    <div class="inner-bg">
-      <div class="container">
-        <div class="col-md-10 col-md-offset-1">
-          <div class="banner-text text-center"> <div class="logo">Decorati</div>
-            <h1>Interior Design</h1>
-            <!--Countdown -->
-            <!--div id="countdown"></div>
-            <!--Countdown --> 
-            <!--a href="#subscribes" class="btn">Notify me</a-->
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
+@include('include.navbar')
 <!--header section --> 
 <!--intro section -->
 <section id="intro" class="section intro">
   <div class="container">
     <div class="row">
-      <div class="col-md-4 col-sm-6">
+      <div class="col-md-12 col-sm-12">
         <div class="intro-content">
-          <h1>We'r forward-thinking agency to build amazing digital products.</h1>
+          <h2 class="text-center">About Us</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
-        
-        <!-- intro --> 
-      </div>
-      <div class="col-md-7  col-md-offset-1 col-sm-6">
-        <div class="intro-content">
-          <h2>Welcome to Orly</h2>
-          <p>Orly is a professional, unique and modern HTML template for a creative agency, portfolio or general business. It can be used as your Coming Soon Page while the main site is under construction.</p>
-        </div>
-        
-        <!-- intro --> 
       </div>
     </div>
   </div>
@@ -74,61 +29,32 @@
 <section id="intro" class="section intro">
   <div class="container">
     <div class="row">
-       <div class="col-xs-4">
-        <div class="card" style="width: 40rem;">
-          <img class="card-img-top" src="./images/produtos/hall/amr7.jpg" alt="Card image cap">
-          <div class="card-block">
-            <h4 class="card-title">Card title</h4>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-        </div>
-      </div>
-     <div class="col-xs-4  ">
-            <div class="card" style="width: 40rem;">
-            <img class="card-img-top" src="./images/produtos/hall/amr7.jpg" alt="Card image cap">
-            <div class="card-block">
-              <h4 class="card-title">Card title</h4>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item">Cras justo odio</li>
-              <li class="list-group-item">Dapibus ac facilisis in</li>
-              <li class="list-group-item">Vestibulum at eros</li>
-            </ul>
-            <div class="card-block">
-              <a href="#" class="card-link">Card link</a>
-              <a href="#" class="card-link">Another link</a>
-            </div>
-       </div>
-     </div>
+    <div class="col-md-12 col-sm-12">
+      <h2 class="text-center">Destaques</h2>
+    </div>
   </div>
+    <div class="row">
+           <div class="col-xs-4">
+            <div class="card" style="width: 40rem;">
+              <img class="card-img-top" src="./images/produtos/hall/amr7.jpg" alt="Card image cap">
+              <div class="card-block">
+                <h4 class="card-title">Movel XPTO</h4>
+              </div>
+            </div>
+          </div>
+          <div class="col-xs-4 col-md-offset-1">
+                <div class="card" style="width: 40rem;">
+                <img class="card-img-top" src="./images/produtos/hall/amr7.jpg" alt="Card image cap">
+                <div class="card-block">
+                  <h4 class="card-title">Movel XPTO2</h4>
+                </div>
+           </div>
+         </div>
+     </div>
 </div>
 </section>
 <!--intro section --> 
-<!--intro section -->
-<section id="intro" class="section intro">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-4 col-sm-6">
-        <div class="intro-content">
-          <h1>We'r forward-thinking agency to build amazing digital products.</h1>
-        </div>
-        
-        <!-- intro --> 
-      </div>
-      <div class="col-md-7  col-md-offset-1 col-sm-6">
-        <div class="intro-content">
-          <h2>Welcome to Orly</h2>
-          <p>Orly is a professional, unique and modern HTML template for a creative agency, portfolio or general business. It can be used as your Coming Soon Page while the main site is under construction.</p>
-        </div>
-        
-        <!-- intro --> 
-      </div>
-    </div>
-  </div>
-</section>
-<!--intro section --> 
+
 <!--subscribe section -->
 <section id="subscribes" class="subscribe">
   <div class="container">
@@ -154,87 +80,6 @@
   </div>
 </section>
 <!--subscribe section --> 
-<!--subscribe section -->
-<section id="contact" class="contact" >
-
-
-    <div class="row">
-       <div class="col-xs-4">
-            <div style="opacity: 0.6;">
-              
-               <img src='./images/produtos/sofa/02.jpg' style="max-width: 100%; max-height: 340px;">
-            </div>
-       </div>
-       <div class="col-md-4">
-          <h3>Contacte-nos</h3>
-          <form action="#">
-            <div class="row form-group">
-              <div class="col-md-6">
-                <label for="fname">First Name</label>
-                <input type="text" id="fname" class="form-control" placeholder="Your firstname">
-              </div>
-              <div class="col-md-6">
-                <label for="lname">Last Name</label>
-                <input type="text" id="lname" class="form-control" placeholder="Your lastname">
-              </div>
-            </div>
-
-            <div class="row form-group">
-              <div class="col-md-12">
-                <label for="email">Email</label>
-                <input type="text" id="email" class="form-control" placeholder="Your email address">
-              </div>
-            </div>
-
-            <div class="row form-group">
-              <div class="col-md-12">
-                <label for="subject">Subject</label>
-                <input type="text" id="subject" class="form-control" placeholder="Your subject of this message">
-              </div>
-            </div>
-
-            <div class="row form-group">
-              <div class="col-md-12">
-                <label for="message">Message</label>
-                <textarea name="message" id="message" cols="30" rows="10" class="form-control" placeholder="Write us something"></textarea>
-              </div>
-            </div>
-            <div class="form-group text-center">
-              <input type="submit" value="Enviar" class="btn btn-primary">
-            </div>
-
-          </form>   
-        </div>
-        <div class="col-md-4">
-          <div class="gtco-contact-info">
-            <h3>Contact Information</h3>
-            <ul>
-              <li class="address">198 West 21th Street, <br> Suite 721 New York NY 10016</li>
-              <li class="phone"><a href="tel://1234567920">+ 1235 2355 98</a></li>
-              <li class="email"><a href="mailto:info@yoursite.com">info@yoursite.com</a></li>
-              <li class="url"><a href="http://gettemplates.co">gettemplates.co</a></li>
-            </ul>
-          </div>
-
-        </div>
-
-    </div>
-
-
-
-    <div class="row">
-       <div class="col-xs-8">
-            <div id="map"></div>
-       </div>
-        <div class="col-xs-4">
-            <div style="opacity: 0.6;">
-              
-               <img src='./images/produtos/sofa/03.jpg' style="max-width: 100%; max-height: 260px;">
-            </div>
-       </div>
-
-    </div>
-</section>
 
 <!-- Footer section --> 
 @include('include.footer')
@@ -248,21 +93,3 @@
 
 </body>
 </html>
-
-    <script>
-      function initMap() {
-        var uluru = {lat: 41.42157109, lng: -8.44086171};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 18,
-          center: uluru,
-           mapTypeId: 'satellite'
-        });
-        var marker = new google.maps.Marker({
-          position: uluru,
-          map: map
-        });
-      }
-    </script>
-    <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCzIWkQuIBfBDQlkmSJ5uGHTtLv59XkGZo&callback=initMap">
-    </script>
---
+++ resources/views/include/navbar.blade.php
@@ -0,0 +1,37 @@
+<section class="banner" role="banner">
+
+<nav class="gtco-nav" role="navigation">
+    <div class="gtco-container">
+      <div class="row">
+        <div class="col-xs-8 text-center menu-1" style="width: 100% !important;">
+          <ul>
+            <li class="active"><a href="index.html">Home</a></li>
+            <li><a href="about.html">Sobre</a></li>
+            <li><a href="produtos.html">Produtos</a></li>
+            <li><a href="contact.html">Contactos</a></li>
+          </ul>
+        </div>
+      </div>
+      
+    </div>
+  </nav>
+  <div class="banner-area"> 
+    <!-- overlay -->
+    <div class="banner-area-gradient"></div>
+    <!-- overlay -->
+    <div class="inner-bg">
+      <div class="container">
+        <div class="col-md-10 col-md-offset-1">
+          <div class="banner-text text-center"> <div class="logo">Decorati</div>
+            <h1>Interior Design</h1>
+            <!--Countdown -->
+            <!--div id="countdown"></div>
+            <!--Countdown --> 
+            <!--a href="#subscribes" class="btn">Notify me</a-->
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
